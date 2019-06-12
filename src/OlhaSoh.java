d3343de74b3663328118ee5b324b4636419c95c5
Tecco2233 <biel.tecco@gmail.com>
2019-06-12 12:17:07 -0300

    slaparsa

---
+++ src/OlhaSoh.java
@@ -0,0 +1,26 @@
+
+public class OlhaSoh {
+	public double adicao(double n, double n1) {
+		return n + n1;
+	}
+	public double subtracao(double n, double n1) {
+		return n - n1;
+}
+	public double multiplicacao(double n, double n1) {
+		return n * n1;
+}
+	public double divisao(double n, double n1) {
+		return n / n1;
+}
+public static void main(String[] args) {
+	OlhaSoh ex = new OlhaSoh();
+	
+	System.out.println(ex.adicao(10,1));
+	System.out.println(ex.subtracao(10,5));
+	System.out.println(ex.multiplicacao(6,4));
+	System.out.println(ex.divisao(8,2));
+}
+}
+
+
+
